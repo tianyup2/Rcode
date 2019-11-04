4d6f5db7c5f591985d614ec555926d65b6c95bde
tianyup2 <53953235+tianyup2@users.noreply.github.com>
2019-11-04 14:55:59 -0800

    Add files via upload

--- dpreassign.R
+++ dpreassign.R
@@ -1,12 +1,12 @@
 library(dirichletprocess)
 PriorDraw.mvnormal2.my<-function (mdObj, n = 1){
   priorParameters <- mdObj$priorParameters
-  
+  dim.m <- dim(priorParameters$phi0)[1]
   sig <- array(sapply(seq_len(n), function(x) solve(rWishart(1,
                                                              priorParameters$nu0, solve(priorParameters$phi0))[, ,
-                                                                                                               1])),dim=c(2,2,n))
+                                                                                                               1])),dim=c(dim.m,dim.m,n))
   mu <- array(t(mvtnorm::rmvnorm(n,
-                                 priorParameters$mu0, priorParameters$sigma0)),dim=c(1,2,n))
+                                 priorParameters$mu0, priorParameters$sigma0)),dim=c(1,dim.m,n))
   
   theta <- list(mu = mu, sig = sig)
   return(theta)
@@ -42,27 +42,49 @@ DirichletProcessGaussian.my<-function (y, g0Priors = c(0, 1, 1, 1),
   return(dpobj)
 }
 
-Initialise.nonconjugate.my<-function (dpObj, posterior = TRUE, 
-                                      updatePrior = FALSE, m = 3, verbose = TRUE){
-  dpObj$clusterLabels <- rep(1, dpObj$n)
-  dpObj$numberClusters <- 1
-  dpObj$pointsPerCluster <- dpObj$n
-  if (posterior) {
-    post_draws <- PosteriorDraw(dpObj$mixingDistribution, 
-                                dpObj$data, 1000)
-    if (verbose) 
-      cat(paste("Accept Ratio: ", length(unique(c(post_draws[[1]])))/1000, 
-                "\n"))
-    dpObj$clusterParameters <- list(post_draws[[1]][, , 1000, 
-                                                    drop = FALSE], post_draws[[2]][, , 1000, drop = FALSE])
+Initialise_my <- function(dpObj, posterior = TRUE, m=3, verbose=TRUE){
+  UseMethod("Initialise_my", dpObj)
+}
+
+# Maybe it's conjugate
+Initialise_my.nonconjugate <- function(dpObj, m = 100, verbose = TRUE) {
+  dpObj$clusterParameters <- PriorDraw(dpObj, m+1)
+  pstick <- StickBreaking_my(alpha = dpObj$alpha,N = m)
+  pend <- ((1-sum(pstick))>=0)*(1-sum(pstick))
+  pstick <- c(pstick,pend)
+  # labelling:
+  label <- c()
+  for(j in seq_len(dpObj$n)){
+    init.llh <- sapply(seq_len(m+1), 
+                       function(i) return(dnorm(dpObj$response[j],
+                                                dpObj$data[j,]%*%dpObj$clusterParameters[[1]][,,i],
+                                                sqrt(dpObj$clusterParameters[[2]][,,i]))))
+    probs <- init.llh*pstick
+    if(all(probs==0)){
+      probs <- rep(1,m+1)
+    }
+    label[j] <- sample.int(m+1, 1, prob = probs)
   }
-  else {
-    dpObj$clusterParameters <- PriorDraw(dpObj$mixingDistribution, 
-                                         1)
+  table.label <- table(label)
+  table.length <- length(table.label)
+  clusterlabel <- as.numeric(names(table.label))
+  names(table.label) <- NULL
+  for(j in seq_len(table.length)){
+    indj <- which(label==clusterlabel[j])
+    label[indj] <- j
   }
-  dpObj$nu <- post_draws[[3]]
-  dpObj$phi <- post_draws[[4]]
-  dpObj$m <- m
+  
+  pointspercluster <- table.label
+  dpObj$clusterParameters[[1]] <- array(dpObj$clusterParameters[[1]][,,clusterlabel],
+                                        dim=c(2,1,table.length))
+  dpObj$clusterParameters[[2]] <- array(dpObj$clusterParameters[[2]][,,clusterlabel],
+                                        dim=c(1,1,table.length))
+  dpObj$pointsPerCluster <- pointspercluster
+  dpObj$numberClusters <- table.length
+  dpObj$clusterLabels <- label
+  
+  dpObj$m <- 3
+  
   return(dpObj)
 }
 
@@ -151,10 +173,7 @@ UpdateAlpha.default.my<-function (dpobj){
 }
 
 update_concentration.my<-function (oldParam, n, nParams, priorParameters){
-  x <- rbeta(1, oldParam + 1, n)
-  pi1 <- priorParameters[1] + nParams - 1
-  pi2 <- n * (priorParameters[2] - log(x))
-  pi1 <- pi1/(pi1 + pi2)
+  x <- oldParam/(oldParam+n)
   postParams1 <- priorParameters[1] + nParams
   postParams2 <- priorParameters[2] - log(x)
   if (runif(1) > pi1) {
@@ -164,6 +183,61 @@ update_concentration.my<-function (oldParam, n, nParams, priorParameters){
   return(list(alpha = new_alpha, postpar1 =postParams1,postpar2 =postParams2))
 }
 
+Fit_my.default <- function(dpObj, its, updatePrior = FALSE, progressBar=TRUE) {
+  
+  if (progressBar){
+    pb <- txtProgressBar(min=0, max=its, width=50, char="-", style=3)
+  }
+  
+  alphaChain <- numeric(its)
+  likelihoodChain <- numeric(its)
+  weightsChain <- vector("list", length = its)
+  clusterParametersChain <- vector("list", length = its)
+  priorParametersChain <- vector("list", length = its)
+  labelsChain <- vector("list", length = its)
+  numLabelChain <- vector("numeric",length = its)
+  pointsPerClusterChain <- vector("list",length = its)
+  
+  for (i in seq_len(its)) {
+    
+    dpObj <- ClusterParameterUpdate_my(dpObj)
+    dpObj <- ClusterComponentUpdate_my(dpObj)
+    dpObj <- UpdateAlpha_my(dpObj)
+    
+    alphaChain[i] <- dpObj$alpha
+    weightsChain[[i]] <- dpObj$pointsPerCluster / dpObj$n
+    clusterParametersChain[[i]] <- dpObj$clusterParameters
+    priorParametersChain[[i]] <- dpObj$mixingDistribution$priorParameters
+    labelsChain[[i]] <- dpObj$clusterLabels
+    numLabelChain[i] <- dpObj$numberClusters
+    pointsPerClusterChain[[i]] <- dpObj$pointsPerCluster
+    
+    likelihoodChain[i] <- sum(log(LikelihoodDP_my(dpObj)))
+    
+    # if (updatePrior) {
+    #   dpObj$mixingDistribution <- PriorParametersUpdate(dpObj$mixingDistribution,
+    #                                                     dpObj$clusterParameters)
+    # }
+    if (progressBar){
+      setTxtProgressBar(pb, i)
+    }
+  }
+  
+  dpObj$weights <- dpObj$pointsPerCluster / dpObj$n
+  dpObj$alphaChain <- alphaChain
+  dpObj$likelihoodChain <- likelihoodChain
+  dpObj$weightsChain <- weightsChain
+  dpObj$clusterParametersChain <- clusterParametersChain
+  dpObj$priorParametersChain <- priorParametersChain
+  dpObj$labelsChain <- labelsChain
+  dpObj$numLabelChain <- numLabelChain
+  dpObj$pointsPerClusterChain <- pointsPerClusterChain
+  
+  if (progressBar) {
+    close(pb)
+  }
+  return(dpObj)
+}
 
 assignInNamespace("PriorDraw.mvnormal2",PriorDraw.mvnormal2.my,"dirichletprocess")
 assignInNamespace("ClusterParameterUpdate.nonconjugate",ClusterParameterUpdate.nonconjugate.my,
@@ -177,7 +251,9 @@ assignInNamespace("GaussianMixtureCreate",GaussianMixtureCreate.my,
 assignInNamespace("PriorDraw.normal",PriorDraw.normal.my,
                   "dirichletprocess")
 
-assignInNamespace("Initialise.nonconjugate",Initialise.nonconjugate.my,
+assignInNamespace("Initialise",Initialise_my,
+                  "dirichletprocess")
+assignInNamespace("Initialise.nonconjugate",Initialise_my.nonconjugate,
                   "dirichletprocess")
 
 assignInNamespace("PosteriorDraw.mvnormal2",PosteriorDraw.mvnormal2.my,
@@ -189,3 +265,7 @@ assignInNamespace("update_concentration",update_concentration.my,
                   "dirichletprocess")
 assignInNamespace("UpdateAlpha.default",UpdateAlpha.default.my,
                   "dirichletprocess")
+
+assignInNamespace("Fit.default",Fit_my.default,
+                  "dirichletprocess")
+
